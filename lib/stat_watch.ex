f1990f0f0891daff4cf26aa8d87f0a41ae06610b
Thomas Johnson <tjohnson1106@gmail.com>
2018-09-28 14:19:52 -0400

    created colum  join function for statistics queries

--- lib/stat_watch.ex
+++ lib/stat_watch.ex
@@ -1,8 +1,12 @@
 defmodule StatWatch do
+  def column_names() do
+    Enum.join(~w(DateTime Subscribers Videos Views), ",")
+  end
+
   def stat_url do
-    youtube_api_v3 = "https://developers.google.com/youtube/v3"
+    youtube_api_v3 = "https://www.googleapis.com/youtube/v3/"
     channel = "id=" <> "UCThRUP0dBolJaHFoSoq07ZA"
-    key = "key=" <> "AIzaSyDjR38yUhrzECXZ8jCsxM_AB-raYh82eUE"
+    key = "key=" <> "AIzaSyANaUj0ClHpbPdmpvMJ6OyGCkbGmP7X-pY"
     "#{youtube_api_v3}channels?#{channel}&#{key}&part=statistics"
   end
 end
