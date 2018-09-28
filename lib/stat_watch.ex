1243ef015c6c24d617efdcea6ecd29c3b56a99df
Thomas Johnson <tjohnson1106@gmail.com>
2018-09-28 14:47:27 -0400

    created fetch stats function

--- lib/stat_watch.ex
+++ lib/stat_watch.ex
@@ -3,7 +3,26 @@ defmodule StatWatch do
     Enum.join(~w(DateTime Subscribers Videos Views), ",")
   end
 
-  def stat_url do
+  def fetch_stats() do
+    now = DateTime.to_string(%{DateTime.utc_now() | microsecond: {0, 0}})
+
+    %{body: body} = HTTPoison.get!(stats_url())
+
+    %{items: [%{statistics: stats} | _]} =
+      Poison.decode!(body,
+        keys: :atoms
+      )
+
+    [
+      now,
+      stats.subscriberCount,
+      stats.videoCount,
+      stats.viewCount
+    ]
+    |> Enum.join(", ")
+  end
+
+  def stats_url do
     youtube_api_v3 = "https://www.googleapis.com/youtube/v3/"
     channel = "id=" <> "UCThRUP0dBolJaHFoSoq07ZA"
     key = "key=" <> "AIzaSyANaUj0ClHpbPdmpvMJ6OyGCkbGmP7X-pY"
